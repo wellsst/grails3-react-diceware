124d785f8eb0687a949aba9f8523496833404c60
wellsst <wellsst@gmail.com>
2019-08-26 12:19:34 +1000

    grails not running on heroku - see if its BootStrap

--- server/grails-app/init/grails3/react/diceware/BootStrap.groovy
+++ server/grails-app/init/grails3/react/diceware/BootStrap.groovy
@@ -6,17 +6,21 @@ import grails.util.Environment
 class BootStrap {
 
     def init = { servletContext ->
-        log.error "Init diceware app in environment: ${Environment.current.name}"
+        /*log.error "Init diceware app in environment: ${Environment.current.name}"
 
-        log.error servletContext.getRealPath("/")
+        log.error servletContext.getRealPath("/")*/
         //log.error servletContext.getResourcePaths("/")
 
-        // A line looks like: 11214	abyss
-        if (Word.count == 0) { // populate DB once only kludge
-            servletContext.getResourceAsStream('diceware.wordlist.asc.txt').text.eachLine { line ->
-                List fields = line.tokenize()
-                new Word(rollKey: fields[0], theWord: fields[1]).save()
+        try {
+// A line looks like: 11214	abyss
+            if (Word.count == 0) { // populate DB once only kludge
+                servletContext.getResourceAsStream('diceware.wordlist.asc.txt').text.eachLine { line ->
+                    List fields = line.tokenize()
+                    new Word(rollKey: fields[0], theWord: fields[1]).save()
+                }
             }
+        } catch (all) {
+            all.printStackTrace()
         }
     }
     def destroy = {
